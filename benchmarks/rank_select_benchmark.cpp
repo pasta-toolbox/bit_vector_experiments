0fe4eba088fd1601be4abdbce4f7c6e146450ed0
Florian Kurpicz <florian@kurpicz.org>
2022-01-14 22:19:50 +0100

    Test compilability of rank and select data structures needed for benchmark

--- benchmarks/rank_select_benchmark.cpp
+++ benchmarks/rank_select_benchmark.cpp
@@ -1,4 +1,11 @@
+#include <array>
+
 #include <sdsl/bit_vectors.hpp>
+
+#include <sux/bits/Rank9Sel.hpp>
+
+#include <bitmap.h>
+#include <shared.h>
 
 int32_t main(int32_t argc, char const * const argv[]) {
   sdsl::bit_vector bv(10000, 0);
@@ -8,6 +15,12 @@ int32_t main(int32_t argc, char const * const argv[]) {
 
   sdsl::bit_vector::rank_0_type bvr0(&bv);
   sdsl::bit_vector::rank_1_type bvr1(&bv);
+
+  std::array<uint64_t, 1000> array;
+
+  sux::bits::Rank9Sel rs(array.data(), array.size() * 64);
+
+  BitmapPoppy bitmap(array.data(), array.size() * 64);
   
   return 0;
 }
